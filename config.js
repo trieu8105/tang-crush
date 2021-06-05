5125dcc57a94ee81eb86da2f1a1ab73dc73ff924
trieu8105 <85400204+trieu8105@users.noreply.github.com>
2021-06-05 21:55:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    introDesc: `If i say you are the only one, will you believe it?`,
+    btnIntro: 'Bấm vào đây để tiếp tục <3',
+    title: 'Cho anh cơ hội để che chở cho em được không 😙',
+    desc: 'Đừng từ chối nhé, sẽ khôngggg được đâuuuu :3 ',
+    btnYes: 'Em đồng ý <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Vì anh dth và cute :3 <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Anh có thể lôi thôi cả tháng
+    Để cùng em lang thang cả tối.',
+    btnAccept: 'hí hí <3',
+    messLink: ' 'https://www.facebook.com/gaucute147/'
 }
