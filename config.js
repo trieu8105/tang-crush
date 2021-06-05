9f4e863f99375ab4a659ca18ca6037e0c8596c9b
trieu8105 <85400204+trieu8105@users.noreply.github.com>
2021-06-05 21:59:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,17 @@
 const CONFIG = {
-    introTitle: 'Em à!',
+    introTitle: 'Baby à!',
     introDesc: `If i say you are the only one, will you believe it?`,
     btnIntro: 'Bấm vàooo đây để tiếp tục <3',
     title: 'Cho anh cơ hội để che chở cho em được khônggg 😙',
-    desc: 'Đừng từ chối anh nhé, sẽ khoonggg được ddaauuu :v ',
+    desc: 'Đừng từ chối anh nhé, sẽ khônggg được đâuuu :v ',
     btnYes: 'EM ĐỒNG Ý <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới 7 tỉ người nafyy tại saoo em lại thích anh?',
+    question:'Trên thế giới 7 tỉ người nàyyy tại saoo em lại thích anh?',
     btnReply: 'Gửi cho anh <3',
-    reply: '',
+    reply: 'vì anh dth và cute :3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Anh có thể lôi thôi cả tháng.Để cùng nàng lang thang cả tối :3',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/messages/t/100009969582255'
+    messDesc: 'Anh có thể lôi thôi cả tháng.
+    Để cùng nàng lang thang cả tối :3',
+    btnAccept: 'hí hí :3 <3',
+    messLink: 'https://www.facebook.com/gaucute147/'
 }
